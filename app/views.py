31abafca880343770547a361191ec7718fa6c9d8
evantoh <evanmwenda@gmail.com>
2018-01-26 11:02:00 +0300

    add dynamic variables for the  news

--- app/views.py
+++ app/views.py
@@ -4,4 +4,12 @@ from app import app
  #view
 @app.route('/')
 def index():
-    return render_template('index.html')
+    message = 'Hello-Welcome to news site'
+    return render_template('index.html',message = message)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+     '''
+    View news page function that returns the news details page and its data
+    '''
+     return render_template('news.html',id = news_id)
