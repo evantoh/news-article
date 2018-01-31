79b11b3812eb1d3a913b1c825f46c9eb7f5985a3
evantoh <evanmwenda@gmail.com>
2018-01-31 22:49:46 +0300

    ident and space the code

--- app/views.py
+++ app/views.py
@@ -1,21 +1,11 @@
-
 from flask import render_template
 from app import app
 from .request import get_sources, get_articles
 
-#  Views
 
 
 @app.route('/')
 def index():
-    """
-    Function that returns the index page and its data
-    Example call
-    top_headlines = get_sources('de','business')
-    We can also have a get_all() function that gets all the news
-    Example call from views would be
-    everything
-    """
     general_list = get_sources('us', 'general')
     business_list = get_sources('us', 'business')
     technology_list = get_sources('us', 'technology')
@@ -23,26 +13,15 @@ def index():
     health_list = get_sources('us', 'health')
     science_list = get_sources('us', 'science')
     entertainment_list = get_sources('us', 'entertainment')
-    test_args = 'Working!'
-    return render_template('index.html',
-                           test_param=test_args,
-                           general=general_list,
-                           business=business_list,
-                           technology=technology_list,
-                           sports=sports_list,
-                           health=health_list,
-                           science=science_list,
-                           entertainment=entertainment_list)
+    test_args = 'Welcome to top headline!'
+    return render_template('index.html',test_param=test_args,general=general_list,business=business_list,technology=technology_list,sports=sports_list,
+health=health_list,science=science_list,entertainment=entertainment_list)
 
 
 @app.route('/news/<id>')
 def news(id):
-    """
-    View articles page that returns the news article from a highlight
-    """
+    
     news_args = get_articles(id)
     highlight_args = 'hulllah you made it here !!'
     # name = f'{results_list}'
-    return render_template('news.html',
-                           highlight_param=highlight_args,
-                           news=news_args)
+    return render_template('news.html', highlight_param=highlight_args,news=news_args)
