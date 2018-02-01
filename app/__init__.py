11e0164a494f976c1eb6a718c65932528ed9ae5e
evantoh <evanmwenda@gmail.com>
2018-02-01 09:34:44 +0300

    register the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,10 @@ def create_app(config_name):
      
      #initializing flask extensions
     bootstrap.init_app(app)
+    #registering blueprint
+
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
      #will add the views and forms
     return app
 
